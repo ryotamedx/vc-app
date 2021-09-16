07c8d2265a74a37514d611a13b90b733de6fa0ea
ryotamedy <ryotamex2@gmail.com>
2021-09-16 16:22:29 +0900

    vc app終了

--- resources/views/message/hello.blade.php
+++ resources/views/message/hello.blade.php
@@ -5,8 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VCApp</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
     <h1>Hello from View!!</h1>
+    <h2>Hello View!!</h2>
 </body>
 </html>
